d21e87aec06664a8298aafa1117f493d8a2f1093
yildirimkadir <yildirimkad34@gmail.com>
2022-08-20 23:56:32 +0200

    "updated"

--- src/components/TutorialList.jsx
+++ src/components/TutorialList.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
 const TutorialList = () => {
-  return <div>TutorialList</div>;
+  return (
+    <div className="container mt-4">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#id</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col">Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default TutorialList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
+import axios from "axios";
 
 const Home = () => {
+  const url = "https://axios-example-cw.herokuapp.com/api/tutorials";
+
+  const getTutorial = async () => {
+    const { data } = await axios.get(url);
+    console.log(data);
+  };
   return (
     <div>
       <AddTutorial />
