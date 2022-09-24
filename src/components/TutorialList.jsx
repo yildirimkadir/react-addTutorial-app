f290e456d35e2a468ee63a6015bb565a102447e3
yildirimkadir <yildirimkad34@gmail.com>
2022-09-24 15:45:43 +0200

    "updated"

--- src/components/TutorialList.jsx
+++ src/components/TutorialList.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { FaEdit } from 'react-icons/fa';
+import { AiFillDelete } from 'react-icons/ai';
+import { useState } from "react";
 
-const TutorialList = ({ tutorials, deleteTutorial }) => {
+const TutorialList = ({ tutorials, deleteTutorial}) => {
+  const [editItem, setEditItem] = useState('');
   return (
     <div className="container mt-4">
       <table class="table table-striped">
@@ -9,21 +13,33 @@ const TutorialList = ({ tutorials, deleteTutorial }) => {
             <th scope="col">#id</th>
             <th scope="col">Title</th>
             <th scope="col">Description</th>
-            <th scope="col">Edit</th>
+            <th scope="col" className="text-center">Edit</th>
           </tr>
         </thead>
         <tbody>
-          {tutorials?.map((item) => {
-            const {title, description, id} = item;
-            return (
-              <>
+          {tutorials.map((item) => {
+            console.log(item);
+            const {id, title, description}= item;
+            return(
                <tr key={id}>
                 <th scope="row">{id}</th>
                 <td>{title}</td>
                 <td>{description}</td>
-                <td>@mdo</td>
+                <td className="text-center text-nowrap">
+                  <FaEdit
+                    data-bs-toggle="modal"
+                    data-bs-target="#edit-modal"
+                    size={20}
+                    className="me-2 text-warning cursor-pointer"
+                    onClick={() => setEditItem(item)}
+                  />
+                  <AiFillDelete
+                    size={22}
+                    className="text-danger cursor-pointer"
+                    onClick={() => deleteTutorial(id)}
+                  />
+                </td>
               </tr>
-            </>
 
             )
           })}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 const Home = () => {
-  const [tutorials, setTutorials] = useState("");
+  const [tutorials, setTutorials] = useState([]);
   const url = "https://axios-example-cw.herokuapp.com/api/tutorials";
 
   const getTutorial = async () => {
